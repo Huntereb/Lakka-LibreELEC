trm2sgm/LibreELEC.tv

TOOLCHAIN: drop $ROOT, convert to absolute path

packages/sysutils/e2fsprogs/package.mk
@@ -116,6 +116,6 @@ makeinstall_host() {
   # Ensure installed files are writeable and not read-only, otherwise future package bumps will fail to overwrite toolchain
   chmod -R +w $(pwd)/.install/usr/*
 
-  cp -Pa $(pwd)/.install/usr/* $ROOT/$TOOLCHAIN
+  cp -Pa $(pwd)/.install/usr/* $TOOLCHAIN
 }
 

Huntereb/Lakka-LibreELEC

packages/sysutils/e2fsprogs/package.mk
@@ -114,6 +114,6 @@ makeinstall_host() {
   rm -fr $(pwd)/.install/bin
   rm -fr $(pwd)/.install/usr/share
 
-  cp -fPa $(pwd)/.install/usr/* $ROOT/$TOOLCHAIN
+  cp -fPa $(pwd)/.install/usr/* $TOOLCHAIN
 }
 